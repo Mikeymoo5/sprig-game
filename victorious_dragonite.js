a213cfe2b276f17367c3d7e49c5f39a159e486cc
Mikeymoo5 <53538532+Mikeymoo5@users.noreply.github.com>
2024-07-18 22:08:45 -0500

    Fixed issues with gameLoop and character objects

--- victorious_dragonite.js
+++ victorious_dragonite.js
@@ -18,39 +18,39 @@ const dirt_sprite = "w";
 
 setLegend(
   [ player_sprite, bitmap`
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................`],
+7777777777777777
+7777777777777777
+7722277777722277
+7720077777700277
+7720077777700277
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7770777777770777
+7770777887770777
+7777000000007777
+7777777777777777
+7777777777777777
+7777777777777777`],
   [ enemy_sprite, bitmap`
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................`],
+3333333333333333
+3330003333000333
+3322230330322233
+3320030330300233
+3320033333300233
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3330000000000333
+3303333388333033
+3303333338333033
+3333333333333333
+3333333333333333`],
   [ grass_sprite, bitmap`
 4444444444444444
 4444444444444444
@@ -87,29 +87,6 @@ CCCCCCCCCCCCCCCC
 CCCCCCCCCCCCCCCC` ]
 );
 
-var player = new Character(3, 3, {
-  "pos": [0, 0], 
-  "direction": "l"
-})
-
-var enemy = new Character(3, 3, {
-  "pos": [0, 0], 
-  "direction": "l"
-})
-
-
-// Stats
-class Character {
-  constructor(max_stamina, max_health, text_info) {
-    this.max_stamina = max_stamina;
-    this.max_health = max_health;
-    this.text_info = text_info
-    
-    this.health = this.max_health
-    this.stamina = this.stamina
-  }
-}
-
 maps = [
    map`
 ..........
@@ -125,10 +102,76 @@ wwwwwwwwww`
 setMap(maps[0]);
 addText("<<<<", {
   x: 0,
-  y: 0,
+  y: 1,
   color: color`3`
 });
 
-function loop() {
+// Stats
+class Character {
+  constructor(max_stamina, max_health, text_info) {
+    this.max_stamina = max_stamina;
+    this.max_health = max_health;
+    this.text_info = text_info
+    
+    this.health = this.max_health
+    this.stamina = this.stamina
+    this.sprite_type = this.text_info["sprite"]
+    addSprite(this.text_info["pos"][0], this.text_info["pos"][1], this.sprite_type)
+
+    // For some reason, addSprite does not return the sprite, so you have to get it with getFirst()
+    this.sprite = getFirst(this.sprite_type)
+  }
+}
+
+var player = new Character(3, 3, {
+  "pos": [1, 4], 
+  "direction": "l",
+  "sprite": player_sprite
+}) ;
+
+var enemy = new Character(3, 3, {
+  "pos": [8, 4], 
+  "direction": "l",
+  "sprite": enemy_sprite
+});
+// Handle player movement
+actionToTake = {
+  "action_type": "none",
+  "action_data": {
+
+  }
+};
+
+onInput("d", () => {
+  actionToTake = {
+    "action_type": "move",
+    "action_data": {
+      "move": 1
+    }
+  }
+});
+
+// The main game loop
+interval = -1
+function gameLoop() {
+  clearText();
+  interval += 1;
+  time = 3 - interval;
+  addText(time.toString(), {
+    x: 10,
+    y: 13,
+    color: color`2`
+  });
+  
+  if(interval >= 3) {
+    interval = -1
+    if(actionToTake["action_type"] == move) {
+      
+      player.x += actionToTake["action_data"]["move"]
+    }
+  }
 
 }
+
+// Run gameLoop once every second
+setInterval(gameLoop, 1000);
